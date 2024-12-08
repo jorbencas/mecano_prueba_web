3b14b50c00b4e30ce90ba8858af7da6537c0170d
jorbencas <jorbencas@gmail.com>
2024-12-08 18:16:00 +0100

    .

--- src/components/TypingArea.tsx
+++ src/components/TypingArea.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface TypingAreaProps {
   text: string;
-  currentIndex: number;
+  currentIndex: number; 
   onKeyPress: (key: string) => void;
   wpm: number;
   accuracy: number;
@@ -12,11 +12,13 @@ interface TypingAreaProps {
 
 const TypingArea: React.FC<TypingAreaProps> = ({ text, currentIndex, onKeyPress, wpm, accuracy, errors, source }) => {
 
+    const textAreaRef = useRef<HTMLParagraphElement>(null);
+
 useEffect(() => {
   const handleKeyPress = (event: KeyboardEvent) => {
     if (event.key === text[currentIndex]) {
       onKeyPress(event.key);
-    }
+    } 
   };
 
   window.addEventListener('keydown', handleKeyPress);
@@ -26,10 +28,20 @@ useEffect(() => {
   };
 }, [onKeyPress]);
 
+useEffect(() => {
+  if (source === 'CreateText' && textAreaRef.current) {
+    textAreaRef.current.style.height = 'auto';
+    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+  }
+}, [text, source]);
+
   return (
     <div className="typing-area">
-      <p className={`text-lg sm:text-xl lg:text-2xl border-2 border-gray-300 rounded-lg p-4 bg-green-100 mb-4 
-        ${source === 'CreateText' ? 'min-h-[6rem] h-auto whitespace-pre-wrap break-words' : ''}`}>
+      <p 
+        ref={textAreaRef}
+        className={`text-lg sm:text-xl lg:text-2xl border-2 border-gray-300 rounded-lg p-4 bg-green-100 mb-4 
+          ${source === 'CreateText' ? 'min-h-[6rem] h-auto whitespace-pre-wrap break-words' : ''}`}
+      >
         {text.split('').map((char, index) => (
           <span key={index} className={
             index < currentIndex ? "text-green-500" :
